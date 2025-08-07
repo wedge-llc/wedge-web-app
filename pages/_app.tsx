68f3b03466fe67d6ba4632779d84528e318d744b
fddiferd <fddiferd@gmail.com>
2025-08-07 00:18:44 -0700

    testing cicd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
+
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
